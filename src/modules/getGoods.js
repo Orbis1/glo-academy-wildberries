dc3a63a60fb1ceb71f4aa2ece0abb07d33fbc100
Orbis <infectores@gmail.com>
2021-11-21 17:58:12 +0300

    task-1

--- src/modules/getGoods.js
+++ src/modules/getGoods.js
@@ -1,14 +1,49 @@
+export const renderGoods = goods => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+    goodsContainer.innerHTML = "";
+
+    goods.forEach(g => {
+        const goodBlock = document.createElement("div");
+
+        goodBlock.classList.add("col-lg-3");
+        goodBlock.classList.add("col-sm-6");
+
+        goodBlock.innerHTML = `
+        <div class="goods-card">
+            <span class="label ${g.label ? null : 'd-none'}">${g.label}</span>
+            <img src="db/${g.img}" alt="image: Hoodie" class="goods-image">
+            <h3 class="goods-title">${g.name}</h3>
+            <p class="goods-description">${g.description}</p>
+            <button class="button goods-card-btn add-to-cart" data-id="${g.id}">
+                <span class="button-price">$${g.price}</span>
+            </button>
+        </div>
+        `;
+
+        goodsContainer.append(goodBlock);
+        console.log(g);
+    });
+};
+
+export const getData = (value, category) => {
+    fetch("https://glo-acadamy-wildberries-default-rtdb.firebaseio.com/db.json")
+        .then(res => res.json())
+        .then(data => {
+            const array = category ? data.filter(g => g[category] === value) : data;
+            localStorage.setItem("goods", JSON.stringify(array));
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = "/goods.html";
+            } else {
+                renderGoods(array);
+            }
+        });
+};
+
+
 export const getGoods = () => {
     const links = document.querySelectorAll(".navigation-link");
     const navigation = document.querySelector("ul.navigation");
     // navigation.addEventListener("click", event => console.dir(event.target.innerText));
-
-    const getData = category => {
-        console.log("load data");
-        fetch("https://glo-acadamy-wildberries-default-rtdb.firebaseio.com/db.json")
-            .then(res => res.json())
-            .then(data => data);
-    };
 
     links.forEach(link =>
         link.addEventListener("click", event => {
@@ -18,13 +53,11 @@ export const getGoods = () => {
                 dataset: { field },
             } = event.target;
 
-            let goods = localStorage.getItem("goods");
-            if (goods === null) {
-                goods = getData();
-                localStorage.setItem("goods", JSON.stringify(data));
-            }
-            const json = JSON.parse(goods);
-            console.log(Object.values(json).filter(g => g[field] === value));
+            getData(value, field);
         })
     );
+
+    if (localStorage.getItem("goods") && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem("goods")));
+    }
 };
--- src/modules/search.js
+++ src/modules/search.js
@@ -1,8 +1,27 @@
+import { renderGoods } from "./getGoods.js";
+
+const getData = (value, category) => {
+    fetch("https://glo-acadamy-wildberries-default-rtdb.firebaseio.com/db.json")
+        .then(res => res.json())
+        .then(data => {
+            const array = data.filter(g => g.name.toLowerCase().includes(value.toLowerCase()));
+
+            localStorage.setItem("goods", JSON.stringify(array));
+
+            if (window.location.pathname !== "/goods.html") {
+                window.location.href = "/goods.html";
+            } else {
+                renderGoods(array);
+            }
+        });
+};
+
 export const search = () => {
-    const input = document.querySelector(".search-block > input");
+    const input = document.querySelector(".form-control > input");
     const search = document.querySelector("#button-addon2");
 
     search.addEventListener("click", () => {
         console.dir(input.value);
+        getData(input.value)
     });
 };
